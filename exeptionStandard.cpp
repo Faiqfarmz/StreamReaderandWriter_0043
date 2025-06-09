1ab2189e81292c13c1e047e401be0978e64aaeed
Faiqfarmz <faiqfarmz@gmail.com>
2025-06-09 10:39:49 +0700

    membuat data awal program

---
+++ exeptionStandard.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+#include <exception>
+//untuk obyek exception yang akan digunakan
+#include <array>
+//untuk obyek array yang akan kita gunakan
+using namespace std;
+
+int main()
+{
+    cout << "Awal Program" << endl; //Penanda 1:...
+    try {
+        array<int, 3> data = { 1, 2, 3 };
+    }
+}
